6aebc45730a7d644d89692c1de441e3954e13a8a
ArunKumarSF4054 <arunkumar.sivakumar@syncfusion.com>
2024-03-19 15:59:42 +0530

    WPF-865510_SamplesAddedForKB

---
+++ Samples/PanTool/PanTool/PanTool/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace PanTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Syncfusion.Windows.Forms.Diagram.Binding binding1 = new Syncfusion.Windows.Forms.Diagram.Binding();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.diagram1 = new Syncfusion.Windows.Forms.Diagram.Controls.Diagram(this.components);
+            this.model1 = new Syncfusion.Windows.Forms.Diagram.Model(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.propertyEditor1 = new Syncfusion.Windows.Forms.Diagram.Controls.PropertyEditor(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.diagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // diagram1
+            // 
+            binding1.DefaultConnector = null;
+            binding1.DefaultNode = null;
+            binding1.Diagram = this.diagram1;
+            binding1.Id = null;
+            //binding1.Label = ((System.Collections.Generic.List<string>)(resources.GetObject("binding1.Label")));
+            binding1.ParentId = null;
+            this.diagram1.Binding = binding1;
+            this.diagram1.Controller.Constraint = Syncfusion.Windows.Forms.Diagram.Constraints.PageEditable;
+            this.diagram1.Controller.DefaultConnectorTool = Syncfusion.Windows.Forms.Diagram.ConnectorTool.OrgLineConnectorTool;
+            this.diagram1.Controller.PasteOffset = new System.Drawing.SizeF(10F, 10F);
+            this.diagram1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.diagram1.EnableTouchMode = false;
+            this.diagram1.HScroll = true;
+            this.diagram1.LayoutManager = null;
+            this.diagram1.Location = new System.Drawing.Point(0, 0);
+            this.diagram1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.diagram1.Model = this.model1;
+            this.diagram1.Name = "diagram1";
+            //this.diagram1.ScrollVirtualBounds = ((System.Drawing.RectangleF)(resources.GetObject("diagram1.ScrollVirtualBounds")));
+            this.diagram1.Size = new System.Drawing.Size(450, 562);
+            this.diagram1.SmartSizeBox = false;
+            this.diagram1.TabIndex = 0;
+            this.diagram1.Text = "diagram1";
+            // 
+            // 
+            // 
+            this.diagram1.View.ClientRectangle = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.diagram1.View.Controller = this.diagram1.Controller;
+            this.diagram1.View.Grid.MinPixelSpacing = 4F;
+            //this.diagram1.View.ScrollVirtualBounds = ((System.Drawing.RectangleF)(resources.GetObject("resource.ScrollVirtualBounds")));
+            this.diagram1.View.ZoomType = Syncfusion.Windows.Forms.Diagram.ZoomType.TopLeft;
+            this.diagram1.VScroll = true;
+            // 
+            // model1
+            // 
+            this.model1.AlignmentType = Syncfusion.Windows.Forms.Diagram.AlignmentType.SelectedNode;
+            this.model1.BackgroundStyle.PathBrushStyle = Syncfusion.Windows.Forms.Diagram.PathGradientBrushStyle.RectangleCenter;
+            this.model1.DocumentScale.DisplayName = "No Scale";
+            this.model1.DocumentScale.Height = 1F;
+            this.model1.DocumentScale.Width = 1F;
+            this.model1.DocumentSize.DisplayName = "SameAsPrinter";
+            this.model1.DocumentSize.Height = 850F;
+            this.model1.DocumentSize.Width = 1100F;
+            this.model1.LineStyle.DashPattern = null;
+            this.model1.LineStyle.LineColor = System.Drawing.Color.Black;
+            this.model1.LogicalSize = new System.Drawing.SizeF(1100F, 850F);
+            this.model1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.model1.ShadowStyle.Color = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.model1.ShadowStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.diagram1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(450, 562);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.propertyEditor1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(450, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(450, 562);
+            this.panel3.TabIndex = 1;
+            // 
+            // propertyEditor1
+            // 
+            this.propertyEditor1.Diagram = this.diagram1;
+            this.propertyEditor1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertyEditor1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.propertyEditor1.Location = new System.Drawing.Point(0, 0);
+            this.propertyEditor1.Name = "propertyEditor1";
+            this.propertyEditor1.Size = new System.Drawing.Size(450, 562);
+            this.propertyEditor1.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.diagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private Syncfusion.Windows.Forms.Diagram.Controls.Diagram diagram1;
+        private Syncfusion.Windows.Forms.Diagram.Model model1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private Syncfusion.Windows.Forms.Diagram.Controls.PropertyEditor propertyEditor1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Samples/PanTool/PanTool/PanTool/Form1.cs
@@ -0,0 +1,101 @@
+﻿using Syncfusion.Windows.Forms.Diagram;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rectangle = Syncfusion.Windows.Forms.Diagram.Rectangle;
+
+namespace PanTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+            this.diagram1.EventSink.NodeMouseEnter += EventSink_NodeMouseEnter;
+            this.diagram1.EventSink.NodeMouseLeave += EventSink_NodeMouseLeave;
+            this.CreateRectangle(200, 150);
+        }
+
+        private void EventSink_NodeMouseLeave(NodeMouseEventArgs evtArgs)
+        {
+            this.diagram1.Controller.ActivateTool("PanTool");
+        }
+
+        private void EventSink_NodeMouseEnter(NodeMouseEventArgs evtArgs)
+        {
+            this.diagram1.Controller.ActivateTool("SelectTool");
+        }
+
+        private void CreateRectangle(float x, float y)
+        {
+            var rectangle = new Rectangle(x, y, 100, 100);
+            rectangle.FillStyle.Type = FillStyleType.Solid;
+            rectangle.FillStyle.Color = Color.CornflowerBlue;
+            rectangle.EnableCentralPort = true;
+
+            var rectangle1 = new Rectangle(x + 50, y + 200, 100, 100);
+            rectangle1.FillStyle.Type = FillStyleType.Solid;
+            rectangle1.FillStyle.Color = Color.CornflowerBlue;
+            rectangle1.EnableCentralPort = true;
+
+            #region CentralPort
+            var port1 = new ConnectionPoint()
+            {
+                //VisualType = PortVisualType.SquarePort,
+                AllowConnectOnDrag = true,
+                OffsetX = 0,
+                OffsetY = 0,
+            };
+            port1.FillStyle.Type = FillStyleType.Solid;
+            port1.FillStyle.Color = Color.Black;
+            rectangle.Ports.Add(port1);
+
+            var port = new ConnectionPoint()
+            {
+                //VisualType = PortVisualType.SquarePort,
+                AllowConnectOnDrag = true,
+                OffsetX = 0,
+                OffsetY = 0,
+            };
+            port.FillStyle.Type = FillStyleType.Solid;
+            port.FillStyle.Color = Color.Black;
+            rectangle1.Ports.Add(port);
+            #endregion
+            OrgLineConnector connector = new OrgLineConnector(new PointF(0, 0), new PointF(0, 0));
+
+
+
+            this.diagram1.Model.AppendChild(rectangle);
+            this.diagram1.Model.AppendChild(rectangle1);
+            rectangle1.CentralPort.TryConnect(connector.HeadEndPoint);
+            rectangle.CentralPort.TryConnect(connector.TailEndPoint);
+            rectangle1.CentralPort.ConnectionsLimit = 2;
+
+            diagram1.Model.AppendChild(connector);
+
+            var custRect = new Rectangle(x + 200, y, 100, 100);
+            custRect.FillStyle.Type = FillStyleType.Solid;
+            custRect.FillStyle.Color = Color.CornflowerBlue;
+            custRect.EnableCentralPort = true;
+
+           
+
+            var custRect2 = new Rectangle(x + 300, y + 200, 100, 100);
+            custRect2.FillStyle.Type = FillStyleType.Solid;
+            custRect2.FillStyle.Color = Color.CornflowerBlue;
+            custRect2.EnableCentralPort = true;
+
+            
+            diagram1.Model.AppendChild(custRect);
+            diagram1.Model.AppendChild(custRect2);
+        }
+    }
+}
