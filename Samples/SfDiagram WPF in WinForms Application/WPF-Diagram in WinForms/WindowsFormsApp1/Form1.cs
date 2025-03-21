fa0c3a385d9c5c61be548f7f4001d6fe67a4c0be
SatishRaj4377 <158574671+SatishRaj4377@users.noreply.github.com>
2025-03-21 17:19:13 +0530

    WF-948005-Sample for Integrating SfDiagram To WinForms

---
+++ Samples/SfDiagram WPF in WinForms Application/WPF-Diagram in WinForms/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media;
+using WPFDiagramLibrary;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            this.CreateDiagramNode();
+            
+        }
+
+        //This method is used for creating a Node using the WPF
+        private void CreateDiagramNode()
+        {
+            SfDiagram diagram = (this.elementHost2.Child as DiagramComponent).View;
+            diagram.Connectors = new ConnectorCollection();
+            (diagram.Info as IGraphInfo).ItemAdded += MainWindow_ItemAdded;
+            NodeViewModel node = new NodeViewModel()
+            {
+                OffsetX = 200,
+                OffsetY = 200,
+                UnitHeight = 100,
+                UnitWidth = 100,
+                Shape = new RectangleGeometry() { Rect = new Rect(100, 100, 100, 100) }
+            };
+            (diagram.Nodes as NodeCollection).Add(node);
+            NodeViewModel node1 = new NodeViewModel()
+            {
+                OffsetX = 300,
+                OffsetY = 400,
+                UnitHeight = 100,
+                UnitWidth = 100,
+                Shape = new RectangleGeometry() { Rect = new Rect(100, 100, 100, 100) }
+
+
+            };
+
+            (diagram.Nodes as NodeCollection).Add(node1);
+            NodeViewModel node2 = new NodeViewModel()
+            {
+                OffsetX = 600,
+                OffsetY = 400,
+                UnitHeight = 100,
+                UnitWidth = 100,
+                Shape = new RectangleGeometry() { Rect = new Rect(100, 100, 100, 100) }
+
+
+            };
+
+            (diagram.Nodes as NodeCollection).Add(node2);
+            NodeViewModel node3 = new NodeViewModel()
+            {
+                OffsetX = 600,
+                OffsetY = 600,
+                UnitHeight = 100,
+                UnitWidth = 100,
+                Shape = new RectangleGeometry() { Rect = new Rect(100, 100, 100, 100) }
+
+
+            };
+
+            (diagram.Nodes as NodeCollection).Add(node3);
+
+            ConnectorViewModel nodeToNodeConnection = new ConnectorViewModel()
+            {
+                SourceNode = node,
+                TargetNode = node1,
+
+
+            };
+
+            //Adding connector into Collection
+            (diagram.Connectors as ConnectorCollection).Add(nodeToNodeConnection);
+            ConnectorViewModel nodeToNodeConnection1 = new ConnectorViewModel()
+            {
+                SourceNode = node2,
+                TargetPoint = new Point(400, 400)
+
+
+
+            };
+
+            //Adding connector into Collection
+            (diagram.Connectors as ConnectorCollection).Add(nodeToNodeConnection1);
+            ConnectorViewModel nodeToNodeConnection2 = new ConnectorViewModel()
+            {
+                SourcePoint = new Point(800, 800),
+                TargetNode = node3,
+
+
+            };
+
+            //Adding connector into Collection
+            (diagram.Connectors as ConnectorCollection).Add(nodeToNodeConnection2);
+          
+        }
+        private void MainWindow_ItemAdded(object sender, ItemAddedEventArgs args)
+        {
+            if (args.Item is ConnectorViewModel)
+            {
+                if ((args.Item as ConnectorViewModel).SourceNode != null)
+                {
+                    (args.Item as ConnectorViewModel).Constraints = (args.Item as ConnectorViewModel).Constraints.Remove(ConnectorConstraints.SourceDraggable);
+                }
+                if ((args.Item as ConnectorViewModel).TargetNode != null)
+                {
+                    (args.Item as ConnectorViewModel).Constraints = (args.Item as ConnectorViewModel).Constraints.Remove(ConnectorConstraints.TargetDraggable);
+                }
+            }
+        }
+
+    }
+}
